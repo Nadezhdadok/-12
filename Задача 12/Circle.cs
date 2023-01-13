970b538eeac20c3364642908c3809388dffedcd9
homeworkdome <homeworkdome@outlook.com>
2023-01-13 17:05:48 +0300

    Добавьте файлы проекта.

---
+++ Задача 12/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_12
+{
+    static internal class Circle
+    {
+        static public double GetLength(double r)
+        {
+            return 2*Math.PI * r;
+        }
+        static public double GetSquare(double r)
+        {
+            return Math.PI *r*r;
+        }
+        static public double GetTochka(double x, double y)
+        {
+            return Math.Sqrt(x*x+y*y);
+        }
+                   
+    }
+}
---
+++ Задача 12/Program.cs
@@ -0,0 +1,18 @@
+﻿using Задача_12;
+Console.WriteLine("Введите радиус окружности");
+double r = Convert.ToDouble(Console.ReadLine());
+double length = Circle.GetLength(r);
+double square = Circle.GetSquare(r);
+Console.WriteLine($"Длина окружности равна {length}, а площадь - \n{square}");
+Console.WriteLine("Введите координату точки по оси х");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату точки по оси у");
+double y = Convert.ToDouble(Console.ReadLine());
+double tochka = Circle.GetTochka(x, y);
+Console.WriteLine($"Расстояние от центра окружности до точки {tochka}");
+if (tochka > r)
+{
+    Console.WriteLine("Заданная точка не принадлежит кругу");
+}
+else
+    Console.WriteLine("Заданная точка принадлежит кругу");
